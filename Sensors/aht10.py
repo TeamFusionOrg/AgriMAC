12057f2b94108f2108daa05ef0234a41057af38d
Saliya <dinushasaliya@gmail.com>
2022-07-21 00:39:50 +0530

    add comments about the muliplexer process

--- Sensors/aht10.py
+++ Sensors/aht10.py
@@ -3,7 +3,7 @@ import adafruit_ahtx0
 import busio
 
 
-def ReadTempAndHum(sclPin,sdaPin,A0,A1,A2):
+def ReadTempAndHum(sclPin,sdaPin,A0,A1,A2):## 000,001,010,.... --> 1 st sensir, 2nd sensor .....
     i2c = busio.I2C(sclPin,sdaPin)  
     sensor = adafruit_ahtx0.AHTx0(i2c)
     
