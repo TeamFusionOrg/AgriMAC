c8f60109625675b16e0b58cef657fa408d2a5650
Saliya <dinushasaliya@gmail.com>
2022-07-21 00:37:59 +0530

    Develop the function to compatible with the I2C multiplexer

--- Sensors/aht10.py
+++ Sensors/aht10.py
@@ -3,7 +3,7 @@ import adafruit_ahtx0
 import busio
 
 
-def ReadTempAndHum(sclPin,sdaPin):
+def ReadTempAndHum(sclPin,sdaPin,A0,A1,A2):
     i2c = busio.I2C(sclPin,sdaPin)  
     sensor = adafruit_ahtx0.AHTx0(i2c)
     
