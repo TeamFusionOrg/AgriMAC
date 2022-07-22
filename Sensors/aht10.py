47f9b06855ee2ac77bbd4e3526ef2710dd9f322a
Saliya <dinushasaliya@gmail.com>
2022-07-22 23:51:40 +0530

    Add multiplexer modules

--- Sensors/aht10.py
+++ Sensors/aht10.py
@@ -1,11 +1,16 @@
 import time
 import adafruit_ahtx0
 import busio
+import RPi.GPIO as gpio
+import adafruit_tca9548a
 
-
-def ReadTempAndHum(sclPin,sdaPin,A0,A1,A2):## 000,001,010,.... --> 1 st sensir, 2nd sensor .....
-    i2c = busio.I2C(sclPin,sdaPin)  
-    sensor = adafruit_ahtx0.AHTx0(i2c)
-    
+# define globals
+SCL_pin = 3
+SDA_pin = 2
+#creating objects
+i2c = busio.I2C(SCL_pin, SDA_pin)
+tca = adafruit_tca9548a.TCA9548A(i2c)
+def ReadTempAndHum(senseNum):
+    sensor = adafruit_ahtx0.AHTx0(tca[senseNum])
     return sensor.temperature, sensor.relative_humidity
 
